f4c7193d2a9937186d1baa061d7e62b7582a27a1
Matt Trifilo <matt.trifilo@gmail.com>
2026-02-22 11:49:14 -0700

    decant v0.6.1

--- Formula/decant.rb
+++ Formula/decant.rb
@@ -2,22 +2,22 @@ class Decant < Formula
   desc "Clean noisy HTML into compact markdown for LLM context"
   homepage "https://github.com/mtrifilo/decant"
   license "MIT"
-  version "0.6.0"
+  version "0.6.1"
 
-  url "https://github.com/mtrifilo/decant/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "9d292e3e95afe061cc84fb5f4b338595f5fde64654e4c4b59a7fa3b146b9c5c4"
+  url "https://github.com/mtrifilo/decant/archive/refs/tags/v0.6.1.tar.gz"
+  sha256 "76ada0365ecb98b5b98adb1d1cad51b2cb5129ce6dae8e34578a2ffa00b62286"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mtrifilo/decant/releases/download/v0.6.0/decant-darwin-arm64"
-      sha256 "a976a3a3de26dd7a3c8f49871df697a9bbc3a3e3875f8e0b99a59ab588874e4d"
+      url "https://github.com/mtrifilo/decant/releases/download/v0.6.1/decant-darwin-arm64"
+      sha256 "7c2eb5100d362f98092f5896db956929b80ca203939d95a82d0b1e14b0e5d631"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/mtrifilo/decant/releases/download/v0.6.0/decant-linux-x64"
-      sha256 "9e92ef88710c27441fccfaa7a7db457fd46665e69538ca28cb2c09cc90cb183c"
+      url "https://github.com/mtrifilo/decant/releases/download/v0.6.1/decant-linux-x64"
+      sha256 "301c34add05bb9a860646297551e2ff81c5f5881c3b263a3c50ccf258b8babb6"
     end
   end
 
